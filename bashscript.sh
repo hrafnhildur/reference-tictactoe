031018c1ae3d0fb537a8899f4a4cb831a73ecf05
hrafnhildur <hrafnhildurs15@ru.is>
2016-12-09 17:23:13 +0000

    commented bashscript

--- bashscript.sh
+++ bashscript.sh
@@ -5,8 +5,9 @@
 # - runs npm install for the server.
 
 echo Cleaning...
-rm -rf ./dist
+rm -rf ./build 	#removes old build file
 
+# on commit: fetches the commit-message and tags the image with it.
 if [ -z "$GIT_COMMIT" ]; then
   export GIT_COMMIT=$(git rev-parse HEAD)
   export GIT_URL=$(git config --get remote.origin.url)
@@ -17,21 +18,23 @@ export GITHUB_URL=$(echo $GIT_URL | rev | cut -c 5- | rev)
 
 
 echo Building app
-npm run build
+npm run build 	# run build....
 
 rc=$?
-if [[ $rc != 0 ]] ; then
+if [[ $rc != 0 ]] ; then # if build fails, display message
     echo "Npm build failed with exit code " $rc
     exit $rc
 fi
 
-mkdir dist
-cat > ./dist/githash.txt <<_EOF_
+mkdir build # create a new build directory
+
+# put the tag on last commit in a temporary txt-file
+cat > ./build/githash.txt <<_EOF_
 $GIT_COMMIT
 _EOF_
 
-mkdir dist/public
-cat > ./dist/public/version.html << _EOF_
+mkdir build/public
+cat > ./build/public/version.html << _EOF_ 	#html doc created
 <!doctype html>
 <head>
    <title>App version information</title>
@@ -44,7 +47,7 @@ cat > ./dist/public/version.html << _EOF_
 </body>
 _EOF_
 
-
+# copy the Dockerfile, package.json and the helperscript into the build dir
 cp ./Dockerfile ./build/
 cp ./package.json ./build/
 cp docker-run.sh ./build/
@@ -52,15 +55,15 @@ cp docker-run.sh ./build/
 cd build
 echo Building docker image
 
-sudo docker build -t hrafnhildurs/tictactoe .
+sudo docker build -t hrafnhildurs/tictactoe . # build the docker image
 
 rc=$?
 if [[ $rc != 0 ]] ; then
-    echo "Docker build failed " $rc
+    echo "Docker build failed " $rc #display message on failure
     exit $rc
 fi
 
-sudo docker push hrafnhildurs/tictactoe
+sudo docker push hrafnhildurs/tictactoe #push the image and display msg if failure
 rc=$?
 if [[ $rc != 0 ]] ; then
     echo "Docker push failed " $rc
