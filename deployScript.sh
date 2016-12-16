244882a1409714ca95c9d61b8e1bb49dd003e664
hrafnhildur <hrafnhildurs15@ru.is>
2016-12-16 15:08:07 +0000

    changed location of key in jenkins

--- deployScript.sh
+++ deployScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -i "var/lib/jenkins/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/docker-compose.yaml ec2-user@54.218.99.220:~/docker-compose.yaml
+scp -o StrictHostKeyChecking=no -i "my-ec2-key.pem" /var/lib/jenkins/workspace/commit/docker-compose.yaml ec2-user@54.218.99.220:~/docker-compose.yaml
 
-scp -o StrictHostKeyChecking=no -i "var/lib/jenkins/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/build/.env ec2-user@54.218.99.220:~/.env
+scp -o StrictHostKeyChecking=no -i "my-ec2-key.pem" /var/lib/jenkins/workspace/commit/build/.env ec2-user@54.218.99.220:~/.env
 
-ssh -i var/lib/jenkins/my-ec2-key.pem ec2-user@54.218.99.220 docker-compose up -d
+ssh -i my-ec2-key.pem ec2-user@54.218.99.220 docker-compose up -d
