18457a3e673c16608c237fcba5acf25b1ee7a1fd
hrafnhildur <hrafnhildurs15@ru.is>
2016-12-16 15:43:49 +0000

    put sudo in script deploy

--- deployScript.sh
+++ deployScript.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/docker-compose.yaml ec2-user@54.218.99.220:~/docker-compose.yaml
+sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/docker-compose.yaml ec2-user@54.218.99.220:~/docker-compose.yaml
 
-scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/build/.env ec2-user@54.218.99.220:~/.env
+sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/build/.env ec2-user@54.218.99.220:~/.env
 
 ssh -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" ec2-user@54.218.99.220 docker-compose up -d
