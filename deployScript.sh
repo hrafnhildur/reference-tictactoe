41468a6a6d9b53871b13281809e869f683dbc527
hrafnhildur <hrafnhildurs15@ru.is>
2016-12-16 15:48:18 +0000

    added one more sudo in script

--- deployScript.sh
+++ deployScript.sh
@@ -4,4 +4,4 @@ sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/deploy-ticta
 
 sudo scp -o StrictHostKeyChecking=no -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/build/.env ec2-user@54.218.99.220:~/.env
 
-ssh -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" ec2-user@54.218.99.220 docker-compose up -d
+sudo ssh -i "/var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" ec2-user@54.218.99.220 docker-compose up -d
