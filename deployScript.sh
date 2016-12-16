bbceb20dbe362b970510a8dbedf91af40ac70251
hrafnhildur <hrafnhildurs15@ru.is>
2016-12-16 15:25:38 +0000

    fix indeployscr.

--- deployScript.sh
+++ deployScript.sh
@@ -4,4 +4,4 @@ scp -o StrictHostKeyChecking=no -i "var/lib/jenkins/workspace/deploy-tictactoe/m
 
 scp -o StrictHostKeyChecking=no -i "var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" /var/lib/jenkins/workspace/commit/build/.env ec2-user@54.218.99.220:~/.env
 
-ssh -i my-ec2-key.pem ec2-user@54.218.99.220 docker-compose up -d
+ssh -i "var/lib/jenkins/workspace/deploy-tictactoe/my-ec2-key.pem" ec2-user@54.218.99.220 docker-compose up -d
