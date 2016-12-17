931b62909839aaa79978573a578ab4f036cbb940
hrafnhildur <hrafnhildurs15@ru.is>
2016-12-17 13:23:12 +0000

    fullgamejoin unit test added, md file for testEx and adding a column in database file created

--- server/socket-app/tictactoe/tictactoe-game.spec.js
+++ server/socket-app/tictactoe/tictactoe-game.spec.js
@@ -125,10 +125,41 @@ describe('join game command', function () {
 
     });
 
-    it('should emit FullGameJoinAttempted event when game full..implement this', function () {
-
-        expect(true).toBe(false);
+    it('should emit FullGameJoinAttempted event when game full', function (){
+        given = [{
+            type: "GameCreated",
+            user: {
+                userName: "TheGuy"
+            },
+            name: "TheFirstGame",
+            timeStamp: "2014-12-02T11:29:29"
+        },
+        {
+            type: "GameJoined",
+            user: {
+                userName: "Gummi"
+            },
+            name: "TheFirstGame",
+            timeStamp: "2014-12-02T11:29:29"
+        }];
+        when {
+                type: "JoinGame",
+                user: {
+                    userName: "Kalli"
+                },
+                name: "TheFirstGame",
+                timeStamp: "2014-12-02T11:29:29",
+                side:'O'
+            };
+        then = [
+            {
+                type: "FullGameJoinAttempted",
+                user: {
+                    userName: "Palli"
+                },
+                name: "TheFirstGame",
+                timeStamp: "2014-12-02T11:30:29"
+            }];
     });
 });
-
 
