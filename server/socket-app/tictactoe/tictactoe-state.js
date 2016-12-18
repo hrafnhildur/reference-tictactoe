b86d0c864a03fdc67f90c3befe9e518955182d88
hrafnhildur <hrafnhildurs15@ru.is>
2016-12-18 13:56:24 +0000

    minor fix(indent...)

--- server/socket-app/tictactoe/tictactoe-state.js
+++ server/socket-app/tictactoe/tictactoe-state.js
@@ -4,27 +4,27 @@ module.exports = function (injected) {
 
     return function (history) {
 
-        var gameFull = false; //in beginning game not full
+        var isFull = false; //in beginning game not full
         var grid = new Array(9); //Gameboard exists as 3x3 array
         var playersTurn = 'X';
 
         function processEvent(event) {
             if(event.type == "GameJoined") { //if one joins game, change stateto fullGame
-                gameFull = true;
+                isFull = true;
             }
 
             if(event.type == "MovePlaced") {
                 grid[event.pos] = event.side //make mark
+                swapPlayers();
             }
-            swapPlayers();
         }
 
         function processEvents(history) {
              _.each(history, processEvent);
         }
  
-         function gameFull() {
-            return gameFull;
+        function gameFull() {
+            return isFull;
         }
 
         function occupiedPos(pos) {
